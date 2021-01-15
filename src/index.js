287488a34ca20e40b637d7b9f8cf9756dffd5f59
namu5 <jakeim11@namuplanet.com>
2021-01-15 13:29:21 +0900

    승자를 결정하는 calculateWinner()함수 추가

--- src/index.js
+++ src/index.js
@@ -44,6 +44,11 @@ class Board extends React.Component {
     // 1. 데이터 값을 직접 변경
     // 2. 원하는 변경 값을 가진 새로운 사본으로 데이터 교체
     const squares = this.state.squares.slice();
+    console.log(calculateWinner(squares) );
+    if( calculateWinner(squares) || squares[i]) {
+        return;
+    }
+
     squares[i] = this.state.xIsNext ? "X" : "O";
     this.setState({
       squares: squares,
@@ -62,7 +67,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner) {
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -106,3 +117,28 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(<Game />, document.getElementById("root"));
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] === squares[b] && squares[b] === squares[c]) {
+      return squares[a];
+    }
+    // if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+    //   return squares[a];
+    // }
+  }
+
+  return null;
+}
