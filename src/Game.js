32a56c872999c73f3756e09c7956f6eb67c9dd54
namu5 <jakeim11@namuplanet.com>
2021-01-19 13:45:58 +0900

    대각선 체크 로직 추가

--- src/Game.js
+++ src/Game.js
@@ -8,8 +8,8 @@ const initBoard = (row = 3, col = 3) => {
 
 const Game = () => {
   const [currentTurn, setCurrentTurn] = useState("X");
-  const [boardDataArr, setBoardDataArr] = useState(initBoard(4, 4));
-  const [curCoordinate, setCurCoordinate] = useState({x: 0, y: 0});
+  const [boardDataArr, setBoardDataArr] = useState(initBoard(5, 5));
+  const [curCoordinate, setCurCoordinate] = useState({x: -1, y: -1});
   const [nextTurn, setNextTurn] = useState("O");
   const [winnerCheck, setWinnerCheck] = useState(false);
 
@@ -34,23 +34,35 @@ const Game = () => {
   };
 
   useEffect(() => {
+    if(curCoordinate.x === -1 && curCoordinate.y === -1) {
+      return;
+    }
+
     if(!winnerCheck) {
       if(winnerCheckValue(boardDataArr, curCoordinate.x, curCoordinate.y)) {
-        console.log("승자 결정");
         setWinnerCheck(true);
       } else {
-        console.log("xxxxx");
         setWinnerCheck(false);
       }
     }
   }, [curCoordinate]);
+
+  
+  const winnerCheckValue = (boardDataArr, x, y) => {
+    const hrizonMatchCheck = horizontalCheck(boardDataArr, x, y);
+    const varticalMatchCheck = varticalCheck(boardDataArr, x, y);
+    const diagonalMatchCheck = diagonalCheck(boardDataArr, x, y);
+
+    console.log("대각선 체크 : " + diagonalMatchCheck);
+
+    return (hrizonMatchCheck || varticalMatchCheck || diagonalMatchCheck) ? true : false;
+  };
 
   const horizontalCheck = (boardDataArr, x, y) => {
     // 수평 체크 x축 고정
     let horizontalLength = boardDataArr[x].length;
     for (let i=0; i<horizontalLength-1; i++) {
       if ((boardDataArr[x][i] !== boardDataArr[x][i+1]) || boardDataArr[x][i] === null) {
-        console.log(boardDataArr[x][i], boardDataArr[x][i+1]);
         return false;
       }
     }
@@ -68,19 +80,27 @@ const Game = () => {
     return true;
   };
 
-  const winnerCheckValue = (boardDataArr, x, y) => {
-    const hrizonMatchCheck = horizontalCheck(boardDataArr, x, y);
-    const varticalMatchCheck = varticalCheck(boardDataArr, x, y);
-
-    return (hrizonMatchCheck || varticalMatchCheck) ? true : false;
-  };
-  
- 
-
   const diagonalCheck = (boardDataArr, x, y) => {
     // 대각선 체크
+    const len = boardDataArr.length;
+    console.log(x,y);
+    if(x-y == 0 || x+y == len-1) {
+      for(let i=0; i<len-1; i++) {
+        if ((boardDataArr[i][i] !== boardDataArr[i+1][i+1]) || boardDataArr[i][y] === null) {
+          console.log(boardDataArr[i][i]
+            ,boardDataArr[i+1][i+1]
+            ,boardDataArr[i][i]);
+          return false;
+        }
+        if (boardDataArr[i][len-1-i] !== boardDataArr[i+1][len-1-i-1] || boardDataArr[i][y] === null) {
+          return false;
+        }
+        return true;
+      }
+    } else {
+      return false;
+    }
   };
-
 
   const turnNotification = "현재 차례 : " + currentTurn;
   const winnerNotivication = nextTurn + " 승리 !";
