8c581a3c5421c82df427a68443046528212d8d21
namu5 <jakeim11@namuplanet.com>
2021-01-19 16:58:55 +0900

    대각선 검증 로직 보완

--- src/Game.js
+++ src/Game.js
@@ -8,10 +8,12 @@ const initBoard = (row = 3, col = 3) => {
 
 const Game = () => {
   const [currentTurn, setCurrentTurn] = useState("X");
-  const [boardDataArr, setBoardDataArr] = useState(initBoard(5, 5));
+  const [boardDataArr, setBoardDataArr] = useState(initBoard(3, 3));
   const [curCoordinate, setCurCoordinate] = useState({x: -1, y: -1});
   const [nextTurn, setNextTurn] = useState("O");
   const [winnerCheck, setWinnerCheck] = useState(false);
+  const [stepNumber, setStepNumber] = useState(0);
+
 
   // 게임 컴포넌트가 처음 랜더링 될때 
   // 현재 Turn은 X 이고 다음 Turn은 O로 설정 하고 시작
@@ -25,11 +27,13 @@ const Game = () => {
     if(copyBoardDataArr[x][y] == null && winnerCheck === false) {
       copyBoardDataArr[x][y] = currentTurn;
       setBoardDataArr(copyBoardDataArr);
-      
-       // 현재Turn에 nextTurn으로 셋팅 되어있던 차례를 적용
+
+      // 현재Turn에 nextTurn으로 셋팅 되어있던 차례를 적용
       setCurrentTurn(nextTurn);
       // nextTurn 변경
       setNextTurn((nextTurn === "X") ? "O" : "X");
+
+      setStepNumber(stepNumber+1);
     }
   };
 
@@ -45,15 +49,13 @@ const Game = () => {
         setWinnerCheck(false);
       }
     }
-  }, [curCoordinate]);
 
+  }, [curCoordinate]);
   
   const winnerCheckValue = (boardDataArr, x, y) => {
     const hrizonMatchCheck = horizontalCheck(boardDataArr, x, y);
     const varticalMatchCheck = varticalCheck(boardDataArr, x, y);
     const diagonalMatchCheck = diagonalCheck(boardDataArr, x, y);
-
-    console.log("대각선 체크 : " + diagonalMatchCheck);
 
     return (hrizonMatchCheck || varticalMatchCheck || diagonalMatchCheck) ? true : false;
   };
@@ -83,36 +85,38 @@ const Game = () => {
   const diagonalCheck = (boardDataArr, x, y) => {
     // 대각선 체크
     const len = boardDataArr.length;
-    console.log(x,y);
-    if(x-y == 0 || x+y == len-1) {
+    if(x-y === 0) {
       for(let i=0; i<len-1; i++) {
-        if ((boardDataArr[i][i] !== boardDataArr[i+1][i+1]) || boardDataArr[i][y] === null) {
-          console.log(boardDataArr[i][i]
-            ,boardDataArr[i+1][i+1]
-            ,boardDataArr[i][i]);
+        // \ 모양 대각선 검증
+        if ((boardDataArr[i][i] !== boardDataArr[i+1][i+1]) || boardDataArr[i][i] === null) {
           return false;
         }
-        if (boardDataArr[i][len-1-i] !== boardDataArr[i+1][len-1-i-1] || boardDataArr[i][y] === null) {
+      }
+    } else if(x+y === len-1) {
+      // / 모양 대각선 검증
+      for(let i=0; i<len-1; i++) {
+        if ((boardDataArr[len-1-i][i] !== boardDataArr[len-1-i-1][i+1]) || boardDataArr[i][len-1-i] === null) {
           return false;
         }
-        return true;
       }
     } else {
       return false;
     }
+
+    return true;
   };
+
 
   const turnNotification = "현재 차례 : " + currentTurn;
   const winnerNotivication = nextTurn + " 승리 !";
-
   return (
     <div className="game">
+      <div>{(winnerCheck) ? winnerNotivication : turnNotification}</div>
       <div className="game-board">
         <Board boardDataArr={boardDataArr} squareClick={squareClick}/>
       </div>
       <div className="game-info">
-        <div>{(winnerCheck) ? winnerNotivication : turnNotification}</div>
-        <ol>{/*test */}</ol>
+        <ol>{stepNumber}</ol>
       </div>
     </div>
   );
