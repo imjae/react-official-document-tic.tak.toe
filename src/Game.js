f39029d99b5364c002f0a678ecb1f5bcfd9bbb43
namu5 <jakeim11@namuplanet.com>
2021-01-27 14:47:26 +0900

    history에 보드, 좌표, 스텝, 턴 데이터 추가, history 이동기능 구현, 승자체크 되돌아가기 안됨

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 import { BOARD_SIZE } from "./Def";
-import { initBoard, convertHistoryArr } from "./Util";
+import { initBoard } from "./Util";
 import { winnerCheckValue } from "./Rules";
 
 import Board from "./Board";
@@ -9,44 +9,63 @@ import History from "./History";
 
 const Game = () => {
   const [currentTurn, setCurrentTurn] = useState("X");
-  const [nextTurn, setNextTurn] = useState("O");
+  // const [nextTurn, setNextTurn] = useState("O");
   const [boardDataArr, setBoardDataArr] = useState(initBoard(BOARD_SIZE));
   const [curCoordinate, setCurCoordinate] = useState({ x: -1, y: -1 });
   const [winnerCheck, setWinnerCheck] = useState(false);
   const [stepNumber, setStepNumber] = useState(0);
   const [history, setHistory] = useState([]);
+  const [winnerNotificate, setWinnerNotificate] = useState(false);
 
   // 게임 컴포넌트가 처음 랜더링 될때
   // 현재 Turn은 X 이고 다음 Turn은 O로 설정 하고 시작
   const squareClick = (boardDataArr, x, y) => {
-    const copyBoardDataArr = [...boardDataArr];
-    setCurCoordinate({ x: x, y: y });
+    changeCoordinate(x, y);
+    changeHistory();
 
+    const copyBoardDataArr = [...boardDataArr];
     if (copyBoardDataArr[x][y] == null && winnerCheck === false) {
       copyBoardDataArr[x][y] = currentTurn;
+      setStepNumber(stepNumber => stepNumber + 1);
       changeBoard(copyBoardDataArr);
       changeTurn();
-      setStepNumber(stepNumber + 1);
     }
+  };
+
+  const changeCoordinate = (x, y) => {
+    setCurCoordinate({ x: x, y: y });
+  };
+
+  const changeHistory = () => {
+    const tmpArr = JSON.parse(JSON.stringify(history));
+    tmpArr.push(
+      JSON.parse(
+        JSON.stringify({
+          board: boardDataArr,
+          step: stepNumber,
+          turn: currentTurn,
+          coordinate: curCoordinate
+        })
+      )
+    );
+    console.log(tmpArr);
+    setHistory(tmpArr);
   };
 
   const changeBoard = (copyBoardDataArr) => {
     setBoardDataArr(copyBoardDataArr);
-    const tmpArr = JSON.parse(JSON.stringify(history));
-    tmpArr.push(JSON.parse(JSON.stringify(copyBoardDataArr)));
-    setHistory(tmpArr);
   };
 
   const changeTurn = () => {
+    const nextTurn = currentTurn === "X" ? "O" : "X";
     setCurrentTurn(nextTurn);
-    const _nextTurn = nextTurn === "X" ? "O" : "X";
-    setNextTurn(_nextTurn);
   };
 
   useEffect(() => {
     if (curCoordinate.x === -1 && curCoordinate.y === -1) {
       return;
     }
+
     if (!winnerCheck) {
       if (winnerCheckValue(boardDataArr, curCoordinate.x, curCoordinate.y)) {
         setWinnerCheck(true);
@@ -56,23 +75,25 @@ const Game = () => {
     }
   }, [curCoordinate]);
 
-  const turnNotification = "현재 차례 : " + currentTurn;
-  const winnerNotivication = nextTurn + " 승리 !";
+  const turnNotification = <h3>{"현재 차례 : " + currentTurn}</h3>;
+  const winnerNotification = <h3>{currentTurn + " 승리 !"}</h3>;
 
   const jumpToHistory = (stepToJumpIdx) => {
-    // 현제 스텝
-    const changedBoardData = history[stepToJumpIdx];
+    // 현재 스텝
     const changedHistory = history.slice(0, stepToJumpIdx);
 
-    setBoardDataArr(changedBoardData);
-    // setHistory(changedHistory);
+    setBoardDataArr(history[stepToJumpIdx].board);
+    setStepNumber(history[stepToJumpIdx].step);
+    setCurrentTurn(history[stepToJumpIdx].turn);
+    setCurCoordinate(history[stepToJumpIdx].coordinate);
+    console.log(history[stepToJumpIdx].coordinate);
+    setHistory(changedHistory);
 
-    // console.log(convertBoard);
   };
 
   return (
     <div className="game">
-      <div>{winnerCheck ? winnerNotivication : turnNotification}</div>
+      <div>{winnerCheck ? winnerNotification : turnNotification}</div>
       <div className="game-board">
         <Board boardDataArr={boardDataArr} squareClick={squareClick} />
       </div>
